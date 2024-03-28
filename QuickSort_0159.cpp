dee33fca1a1d59426aaf602640c37d9c50d91aaf
fikrimhammd <fikri.kdi60@gmail.com>
2024-03-28 10:50:37 +0800

    element array

--- QuickSort_0159.cpp
+++ QuickSort_0159.cpp
@@ -6,3 +6,12 @@ int arr[20];
 int cmp_count = 0; //number of comparison
 int mov_count = 0; //number of data movements
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array: ";
+		cin >> n;
+
+
+       
