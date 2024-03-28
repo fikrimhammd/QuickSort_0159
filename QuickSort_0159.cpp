067d825d43b02dc7f8e2a28d05cf968a03ce7f72
fikrimhammd <fikri.kdi60@gmail.com>
2024-03-28 11:31:32 +0800

    q_short step 13

--- QuickSort_0159.cpp
+++ QuickSort_0159.cpp
@@ -40,7 +40,7 @@ void swap(int x, int y)
 
 }
 
-void q_short( int low, int high)
+void q_short(int low, int high)
 {
 
 	int temp;
@@ -84,6 +84,16 @@ void q_short( int low, int high)
 
 	//recursive call to sort the left sub array
 	q_short(low, j - 1); //step 12
+
+	//recursive call to sort the right sub array
+
+	q_short(j + 1, high); //step 13
+
+
+}
+
+
+
 
 
 
